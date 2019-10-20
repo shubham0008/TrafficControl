f57b53d037d5bcb333625ed2fa8eee33d01daf9b
shubham0008 <sharma.shubham0008@gmail.com>
2019-10-20 11:58:32 +0530

    Login Activity added.

--- DummyApp/app/src/main/java/com/azuretestapp/dummyapp/MainActivity.java
+++ DummyApp/app/src/main/java/com/azuretestapp/dummyapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.azuretestapp.dummyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+        Button next ;
+        next = findViewById( R.id.button );
+        next.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent( MainActivity.this, MapsActivity.class ) );
+
+            }
+        } );
+
     }
 }
